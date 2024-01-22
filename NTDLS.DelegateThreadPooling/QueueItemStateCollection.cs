08befb5d0fb493abbb0a0216fa6069ff70f7e0e0
Josh Patterson <josh@ntdls.com>
2024-01-22 12:18:23 -0500

    Added collection exception handling

--- NTDLS.DelegateThreadPooling/QueueItemStateCollection.cs
+++ NTDLS.DelegateThreadPooling/QueueItemStateCollection.cs
@@ -31,6 +31,28 @@ namespace NTDLS.DelegateThreadPooling
         }
 
         /// <summary>
+        /// Adds a delegate function to the work queue.
+        /// </summary>
+        /// <param name="parameter">User supplied parameter that will be passed to the delegate function.</param>
+        /// <param name="parameterizedThreadAction">The delegate function to execute when a thread is ready.</param>
+        /// <returns></returns>
+        public QueueItemState Enqueue(object parameter, ParameterizedThreadAction parameterizedThreadAction)
+        {
+            var queueToken = _threadPool.Enqueue(parameter, parameterizedThreadAction);
+            Collection.Add(queueToken);
+            return queueToken;
+        }
+
+        /// <summary>
+        /// Returns true is any of the items have an exception.
+        /// </summary>
+        /// <returns></returns>
+        public bool ExceptionOccured()
+        {
+            return Collection.Any(o => o.ExceptionOccured);
+        }
+
+        /// <summary>
         /// Cancels all queued worker items.
         /// </summary>
         /// <returns>Returns true if all item were cancelled.</returns>
